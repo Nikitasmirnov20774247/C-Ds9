1c208a96caaa53bef8c4a018a81e42cc3c89f63c
Nikita <Nikitasmirnov2000@yandex.ru>
2022-09-07 20:27:30 +0300

    Ds1

---
+++ Ds1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+Console.Write("Введите число m(от): ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число n(до): ");
+int n = int.Parse(Console.ReadLine());
+
+if (n < m)
+{
+    Console.Write("!! Число n не может быть меньше m !!");
+    return;
+}
+
+int WriteNumner(int m, int n)
+{
+    Console.Write(m);
+    if (m != n)
+    {
+        Console.Write(", ");
+    }
+    if(m == n) return n;
+    return WriteNumner(m + 1,n);
+}
+
+Console.WriteLine();
+Console.WriteLine($"Вывод чисел от {m} до {n}:");
+WriteNumner(m,n);
